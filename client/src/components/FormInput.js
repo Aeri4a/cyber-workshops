2566448434b4a6b7c520fd770063eeba1f063450
Aeri4a <80831299+Aeri4a@users.noreply.github.com>
2023-03-19 22:25:01 +0100

    r form valid

---
+++ client/src/components/FormInput.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import styles from "../styles/Forms.module.scss";
+
+function FormInput(props) {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  return (
+    <div className={styles.formInput}>
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={() => setFocused(true)}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
+}
+
+export default FormInput;
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,32 +1,76 @@
 import React, { useState } from "react";
-import styles from "../styles/Register.module.scss";
+import styles from "../styles/Forms.module.scss";
+import FormInput from "./FormInput";
 
 //Notifications
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function Register() {
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character!",
+      label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "password",
+      type: "password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+    },
+    {
+      id: 3,
+      name: "confirmPassword",
+      type: "password",
+      errorMessage: "Passwords don't match!",
+      label: "Confirm password",
+      pattern: values.password,
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+    console.log(values);
+  };
+
   return (
     <div className={styles.container}>
-      <h2>Make an account</h2>
-      <form method="POST">
-        <p className={styles.parg}>Username</p>
-        <input className={styles.inputs} type="text" />
-        <hr />
-        <p className={styles.parg}>Password</p>
-        <input className={styles.inputs} type="password" />
-        <hr />
-        <p className={styles.parg}>Confirm password</p>
-        <input className={styles.inputs} type="password" />
-        <input
-          className={styles.button}
-          type="submit"
-          value="Register"
-          onClick={(e) => {
-            toast.success("Test");
-            e.preventDefault();
-          }}
-        />
+      <form className={styles.mainForm} method="POST" onSubmit={handleSubmit}>
+        <div className={styles.inputs}>
+          <h2>Make an account</h2>
+          {inputs.map((input) => {
+            return (
+              <FormInput
+                key={input.id}
+                {...input}
+                value={values[input.name]}
+                onChange={onChange}
+              />
+            );
+          })}
+        </div>
+        <button>Register</button>
       </form>
       <ToastContainer
         position="top-center"

110949ad91c9063b39a968b908a9887995a77112
Aeri4a <ankaxgry@gmail.com>
2023-03-25 23:05:25 +0100

    base backend

--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const corsSettings = {
+  origin: "http://localhost:3001",
+};
+
+app.use(cors(corsSettings));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json("Server is working");
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
